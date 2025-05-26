6973f1d9893d2b503bf6cf190a0019929ec146b3
Trey Helmer <treyhelmer@gmail.com>
2025-05-26 09:41:15 -0400

    add app js

---
+++ app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Activate the navigation links based on scroll position
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  window.addEventListener("scroll", function () {
+    let current = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop - 150) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href").substring(1) === current) {
+        link.classList.add("active");
+      }
+    });
+  });
+});
