7d5d6b0f66322739defa646a7aa760122b2157ba
Trey Helmer <treyhelmer@gmail.com>
2026-05-22 13:48:43 -0400

    Add root Pages build for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://www.atlantuspartners.com",
+  site: "https://atlantuspartners.com",
   output: "static"
 });
